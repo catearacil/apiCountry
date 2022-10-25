c01701c551e4f78bf34b2a0c71c179e40fa6a0b0
catearacil <caterinaaracil@gmail.com>
2022-10-25 20:31:34 -0300

    finish

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ const axios = require("axios")
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(()=>{loader()}).then(() => { 
-  server.listen(3001, () => {
+  server.listen(process.env.PORT|| 3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
