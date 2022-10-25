83d1c2df07ee3b167511277b543ef78372b5fa41
catearacil <caterinaaracil@gmail.com>
2022-10-25 18:58:09 -0300

    segundo commit

--- src/db.js
+++ src/db.js
@@ -1,14 +1,22 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
+import * as pg from 'pg';
 const path = require('path');
 const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/country`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/ykzpjsul`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialectedModule:pg,
+  dialectedOptions: {
+    ssl: {
+      require:true,
+      rejectUnauthorized: false
+    }
+  },
 });
 const basename = path.basename(__filename);
 
